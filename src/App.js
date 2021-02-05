b8b19af40bf800146f66dde00f4147b9d3be111b
Jules Morelli <jmorelli@manas.tech>
2021-02-05 19:09:08 -0300

    Add constants for paths

--- src/App.js
+++ src/App.js
@@ -7,27 +7,28 @@ import {
 import NewCatto from "./views/NewCatto";
 import Home from "./views/Home";
 import './App.css';
+import { paths } from './utils';
 
 function App() {
   return (
     <Router>
       <div>
-      <header>
-        <Link to="/"><h1>Cattodex</h1></Link>
-        <Link to="/newcatto" className="button">Add your first catto!</Link>
-      </header>
-      <main>
-        <Switch>
-          <Route path="/newcatto">
-            <NewCatto />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </main>
-      <footer>Made with <i class="lab la-gratipay"></i> by <a  href="https://github.com/Kekeberto" target="_blank">zeke</a> & <a href="https://github.com/fornielesv"  taget="_blank">iso</a></footer>
-    </div>
+        <header>
+          <Link to={paths.home}><h1>Cattodex</h1></Link>
+          <Link to={paths.newCatto} className="button">Add your first catto!</Link>
+        </header>
+        <main>
+          <Switch>
+            <Route path={paths.newCatto}>
+              <NewCatto />
+            </Route>
+            <Route path={paths.home}>
+              <Home />
+            </Route>
+          </Switch>
+        </main>
+        <footer>Made with <i class="lab la-gratipay"></i> by <a  href="https://github.com/Kekeberto" target="_blank">zeke</a> & <a href="https://github.com/fornielesv"  taget="_blank">iso</a></footer>
+      </div>
     </Router>
   );
 }
