ba9b93eec4bf241d1b247faf25e86b887477dfbb
Kekebertom <masterofpuppets69@gmail.com>
2021-02-16 19:37:34 -0300

    Primeros cambios a NewCatto

--- src/views/NewCatto.js
+++ src/views/NewCatto.js
@@ -1,7 +1,25 @@
+import './NewCatto.css';
 
 function NewCatto(props) {
   return (
-    'Aca podes crear tu catto! PROXIMAMENTE!!'
+    <section>
+      <form>
+      Aca podras agregar a tu catto!
+        <div>
+          ¿Cuantos gatos tenes?<br></br>
+          <select>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4 o más">4+</option>
+          </select>
+        </div>
+        <div>
+          ¿Como se llama tu gato?<br></br>
+          <textarea></textarea>
+        </div>
+      </form>
+    </section>
   );
 }
 
